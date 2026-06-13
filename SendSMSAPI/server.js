ac873200687fb54d98f29b8a3023e7868c585516
Bhaskar <harshmishra37992@gmail.com>
2026-06-13 20:42:22 +0530

    added a /get route for heath check

--- SendSMSAPI/server.js
+++ SendSMSAPI/server.js
@@ -7,6 +7,9 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.get("/", (req, res) => {
+  res.send("Portfolio Backend Running 🚀");
+});
 
 app.use("/api/contact", require("./routes/contact"));
 
